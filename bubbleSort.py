5ce0ac66adfb25d8a716f197bb65e4c11942c252
Dagim FIkru <dagim@kali.local>
2022-04-09 00:16:06 +0000

    bubble sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,1 +1,13 @@
-print("hello world")
+def listToOrder(list):
+    for i in range(len(list)-1):
+        if list[i] > list[i+1]:
+            list[i]= list[i] + list[i+1]
+            list[i+1] = list[i] - list[i+1]
+            list[i] = list[i] - list[i+1]
+            listToOrder(list)
+def main():
+    list = [10,4,3,7,20,40,120,555,-50]
+    print(list)
+    listToOrder(list)
+    print(list)
+main()
