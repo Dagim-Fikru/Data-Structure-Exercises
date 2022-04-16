0943d32231ba2d028dd7ea0e7b0bdbabf643baad
Dagim Fikru <dagimfikru@gmail.com>
2022-04-16 15:33:40 +0000

    merging two sorted lists

---
+++ merge2sortedLists.py
@@ -0,0 +1,29 @@
+def mergeTwoSortedLists(listA,listB):
+    sortedList = []
+    a = len(listA)
+    b = len(listB)
+    i=j=0
+    while i < a and j < b:
+        if listA[i]<=listB[j]:
+            sortedList.append(listA[i])
+            i+=1
+
+        else:
+            sortedList.append(listB[j])
+            j+=1
+
+    while i<a:
+        sortedList.append(listA[i])
+        i+=1
+    while j<b:
+        sortedList.append(listB[j])
+
+    return sortedList
+
+
+def main():
+    listA = [1,2,3,40,50,100]
+    listB = [20,25,55,95,66,90]
+
+    print(mergeTwoSortedLists(listA,listB))
+main()
